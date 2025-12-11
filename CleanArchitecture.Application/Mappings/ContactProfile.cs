dc9c1c849c6609107515f8781ca1d5381ef54e2e
Saurabh Lodhi <Saurabh.lodhi@monkoutsourcing.com>
2025-12-11 12:16:03 +0530

    remove unecessory commenst and commented code

--- CleanArchitecture.Application/Mappings/ContactProfile.cs
+++ CleanArchitecture.Application/Mappings/ContactProfile.cs
@@ -1,24 +1,4 @@
-﻿//using AutoMapper;
-//using CleanArchitecture.Application.DTOs.Contact;
-//using CleanArchitecture.Domain.Entities;
-
-//namespace CleanArchitecture.Application.Mappings
-//{
-//    /// <summary>
-//    /// AutoMapper profile for Contact Submission mappings.
-//    /// </summary>
-//    public class ContactProfile : Profile
-//    {
-//        public ContactProfile()
-//        {
-//            CreateMap<ContactSubmission, ContactSubmissionDto>()
-//                .ForMember(dest => dest.Country, opt => opt.Ignore());
-
-//            CreateMap<CreateContactSubmissionDto, ContactSubmission>();
-//        }
-//    }
-//}
-
+﻿
 
 using AutoMapper;
 using CleanArchitecture.Application.DTOs.Contact;
